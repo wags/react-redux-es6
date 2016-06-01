c31d43e87194aee136a55f3c4feda471b3baa324
Matt Wagner <matt@wags.us>
2016-06-01 10:26:39 -0400

    Create course page
    Add a page for course information and update the header and routes to
    enable navigation to it.

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
+      <Link to="/courses" activeClassName="active">Courses</Link>
+      {" | "}
       <Link to="/about" activeClassName="active">About</Link>
     </nav>
   );
