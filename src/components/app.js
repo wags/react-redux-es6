8743f612a33912c76854249e6941a7e44b9f5108
Matt Wagner <matt@wags.us>
2016-06-01 10:19:44 -0400

    Create header
    Remove static header text and add a navigation component.

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,12 @@
 // This component handles the App template used on every page.
 import React, {PropTypes} from 'react';
+import Header from './common/Header';
 
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p>Header here...</p>
+        <Header/>
         {this.props.children}
       </div>
     );
