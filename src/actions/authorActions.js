53e08b19c4e7fad464d6dc8d12808a1f9c4f2865
Matt Wagner <matt@wags.us>
2016-06-02 15:13:17 -0400

    Create author actions
    Set up the actions that will be used to work with author data.

---
+++ src/actions/authorActions.js
@@ -0,0 +1,16 @@
+import AuthorApi from '../api/mockAuthorApi';
+import * as types from './actionTypes';
+
+export function loadAuthorsSuccess(authors) {
+  return {type: types.LOAD_AUTHORS_SUCCESS, authors};
+}
+
+export function loadAuthors() {
+  return dispatch => {
+    return AuthorApi.getAllAuthors().then(authors => {
+      dispatch(loadAuthorsSuccess(authors));
+    }).catch(error => {
+      throw(error);
+    });
+  };
+}
