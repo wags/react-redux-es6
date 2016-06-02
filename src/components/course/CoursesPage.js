097aed0d3726a341b48fad97ac0a9383bf868c11
Matt Wagner <matt@wags.us>
2016-06-02 11:12:17 -0400

    Add course list component
    Add a component that handles generating the markup for the list of
    course data.

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
@@ -13,10 +14,12 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+    const {courses} = this.props;
+
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses}/>
       </div>
     );
   }
