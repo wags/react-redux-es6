fddecafc4463d45a6da4c3e023aae4df80f4b2a5
Matt Wagner <matt@wags.us>
2016-06-03 09:14:50 -0400

    Handle creates and updates in course reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -6,6 +6,18 @@ export default function courseReducer(state = initialState.courses, action) {
     case types.LOAD_COURSES_SUCCESS:
       return action.courses;
 
+    case types.CREATE_COURSE_SUCCESS:
+      return [
+        ...state,
+        Object.assign({}, action.course)
+      ];
+
+    case types.UPDATE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.course.id),
+        Object.assign({}, action.course)
+      ];
+
     default:
       return state;
   }
