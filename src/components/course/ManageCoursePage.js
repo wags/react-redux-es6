43d95cd3305e37db9e00b5b77f3b3654df85f3d9
Matt Wagner <matt@wags.us>
2016-06-03 09:27:22 -0400

    Redirect via react router context
    After saving the course, redirect to the course list page.

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -27,6 +27,7 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.props.actions.saveCourse(this.state.course);
+    this.context.router.push('/courses');
   }
 
   render() {
@@ -46,6 +47,11 @@ ManageCoursePage.propTypes = {
   course: PropTypes.object.isRequired,
   authors: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired
+};
+
+// Pull in the React Router context so router is available on this.context.router.
+ManageCoursePage.contextTypes = {
+  router: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
