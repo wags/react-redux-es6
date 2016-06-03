9c37707706b2588a66e72c55f7f7aacf0c98e839
Matt Wagner <matt@wags.us>
2016-06-03 15:25:59 -0400

    Hide preloader based on status
    Configure the header to only show the preloader if an AJAX call is in
    progress. (Refresh the courses page to best see this in action.)

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -2,7 +2,7 @@ import React, {PropTypes} from 'react';
 import { Link, IndexLink } from 'react-router';
 import LoadingDots from './LoadingDots';
 
-const Header = () => {
+const Header = ({loading}) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
@@ -10,9 +10,13 @@ const Header = () => {
       <Link to="/courses" activeClassName="active">Courses</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
-      <LoadingDots interval={100} dots={20}/>
+      {loading && <LoadingDots interval={100} dots={20}/>}
     </nav>
   );
+};
+
+Header.propTypes = {
+  loading: PropTypes.bool.isRequired
 };
 
 export default Header;
